033a4e15b8b111306a18296109d9f80757d781cc
joehecn <leanbrown@live.cn>
2018-07-28 21:05:06 +0800

    U Similar blocks

--- __tests__/robot/webwxapi/index.synccheck.test.js
+++ __tests__/robot/webwxapi/index.synccheck.test.js
@@ -4,6 +4,18 @@ const superagent = require('superagent')
 const webwxapi = require('@/robot/webwxapi')
 const mockAgent = require('../../../helper/mockAgent.js')
 
+const testErr = async (text, status, message) => {
+  expect.assertions(2)
+    
+  superagent.agent = mockAgent({ text })
+  await webwxapi.jslogin()
+
+  webwxapi.synccheck({}, []).catch(error => {
+    expect(error.status).toBe(status)
+    expect(error.message).toBe(message)
+  })
+}
+
 describe('synccheck', () => {
   test('success', async () => {
     expect.assertions(1)
@@ -19,58 +31,22 @@ describe('synccheck', () => {
   })
 
   test('803', async () => {
-    expect.assertions(2)
-    
-    superagent.agent = mockAgent({
-      text: `window.synccheck={retcode:"1100",selector:"0"}`
-    })
-    await webwxapi.jslogin()
-
-    webwxapi.synccheck({}, []).catch(error => {
-      expect(error.status).toBe(803)
-      expect(error.message).toBe('登出微信')
-    })
+    const text = 'window.synccheck={retcode:"1100",selector:"0"}'
+    await testErr(text, 803, '登出微信')
   })
 
   test('804', async () => {
-    expect.assertions(2)
-    
-    superagent.agent = mockAgent({
-      text: `window.synccheck={retcode:"1101",selector:"0"}`
-    })
-    await webwxapi.jslogin()
-
-    webwxapi.synccheck({}, []).catch(error => {
-      expect(error.status).toBe(804)
-      expect(error.message).toBe('其他设备登录web微信')
-    })
+    const text = 'window.synccheck={retcode:"1101",selector:"0"}'
+    await testErr(text, 804, '其他设备登录web微信')
   })
 
   test('805', async () => {
-    expect.assertions(2)
-    
-    superagent.agent = mockAgent({
-      text: `window.synccheck={retcode:"1102",selector:"0"}`
-    })
-    await webwxapi.jslogin()
-
-    webwxapi.synccheck({}, []).catch(error => {
-      expect(error.status).toBe(805)
-      expect(error.message).toBe('暂时不知道')
-    })
+    const text = 'window.synccheck={retcode:"1102",selector:"0"}'
+    await testErr(text, 805, '暂时不知道')
   })
 
   test('806', async () => {
-    expect.assertions(2)
-    
-    superagent.agent = mockAgent({
-      text: `window.synccheck={retcode:"11",selector:"0"}`
-    })
-    await webwxapi.jslogin()
-
-    webwxapi.synccheck({}, []).catch(error => {
-      expect(error.status).toBe(806)
-      expect(error.message).toBe('监听心跳失败')
-    })
+    const text = 'window.synccheck={retcode:"11",selector:"0"}'
+    await testErr(text, 806, '监听心跳失败')
   })
 })
